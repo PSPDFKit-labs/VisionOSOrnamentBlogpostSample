5add4a44e6403e0afae91e7b8eba178aa7c74274
Vishnu <vishnubishnoi7@gmail.com>
2024-03-12 12:15:03 +0530

    Initial Commit

---
+++ VisionOSOrnamentExample/VisionOSOrnamentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSOrnamentExampleApp.swift
+//  VisionOSOrnamentExample
+//
+//  Created by Vishnu Dutt on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSOrnamentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
